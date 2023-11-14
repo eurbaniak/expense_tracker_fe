1893555906c73652ff30145890873944979fd5f9
Edwin <50717284+Adderall-dev@users.noreply.github.com>
2023-11-14 17:24:06 +0100

    add user email display in main page, remove unused about button

--- src/components/Layout/components/Header/index.tsx
+++ src/components/Layout/components/Header/index.tsx
@@ -1,13 +1,16 @@
-import { Group, Burger, Container, Text, Button } from "@mantine/core";
+import { Group, Burger, Container, Text, Button, Divider } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import classes from "./header.module.scss";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../../utils/AuthContext/AuthContext";
+import { useGetCurrentUser } from "../../../../api/queries";
 
 export function Header() {
   const [opened, { toggle }] = useDisclosure(false);
   const navigate = useNavigate();
   const { authenticated } = useAuth();
+  const { data } = useGetCurrentUser();
+  const userEmail = data?.currentUser.email;
 
   return (
     <header className={classes.header}>
@@ -22,15 +25,6 @@ export function Header() {
           >
             Expense Guardian
           </Text>
-          <Group gap={5} visibleFrom="sm">
-            <a
-              href="/"
-              className={classes.link}
-              onClick={(event) => event.preventDefault()}
-            >
-              About
-            </a>
-          </Group>
           <Group gap={5} visibleFrom="sm">
             {!authenticated && (
               <>
@@ -53,14 +47,18 @@ export function Header() {
               </>
             )}
             {authenticated && (
-              <Button
-                variant="subtle"
-                radius={10}
-                onClick={() => navigate("/dashboard")}
-                size="md"
-              >
-                Go to Dashboard
-              </Button>
+              <>
+                <Text>{userEmail}</Text>
+                <Divider orientation="vertical" m={10} size={3} />
+                <Button
+                  variant="light"
+                  radius={10}
+                  onClick={() => navigate("/dashboard")}
+                  size="md"
+                >
+                  Go to Dashboard
+                </Button>
+              </>
             )}
           </Group>
           <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
